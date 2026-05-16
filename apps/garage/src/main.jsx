b47688be0edabc8d1da2b3f0808b89442b9e9b6a
Your Name <stasdodesign@ya.ru>
2026-05-16 23:48:01 +0300

    1

---
+++ apps/garage/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Garage from "./garage.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Garage />
+  </React.StrictMode>
+);
